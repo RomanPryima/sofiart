e453f23424bf3031ba4d18e31903cbe908888c84
rpryima <rpryima@rmn.com>
2017-12-06 22:55:58 +0200

    6.12.17

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Article(models.Model):
+    number = models.CharField(max_length=5, unique=True)
+    name = models.TextField(max_length=45, blank=True)
+    description = models.TextField(max_length=500, blank=True)
+    published = models.DateTimeField(auto_now_add=True, blank=True)
+    creator = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 def home_page(request):
-    return HttpResponse('Hello world!')
+    articles = Article.objects.all()
+    articles_names = list()
+
+    for article in articles:
+        articles_names.append(article.name)
+
+    response_html = '<br>'.join(articles_names)
+
+    return render(request, 'home_page.html', {'articles': articles})
