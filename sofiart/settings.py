f93246a6e0b3f66eea90fd7008a532009d9ff137
Roman Pryima <rpryima@rmn.com>
2018-05-11 13:15:11 +0300

    [11.05.18] set nginx + gunicorn

--- sofiart/settings.py
+++ sofiart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '20a6)&eh@p#l9a22*fzr$c)9%!ul2o#@t(d91ar6-rhb#f=*jm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 INTERNAL_IPS = ('127.0.0.1')
 
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+
     'django.contrib.staticfiles',
     'django_extensions',
     'sofiart',
